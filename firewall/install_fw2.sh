73094e5638a835d9c59d6718612d5bbe51219887
Chris Crow <ccrow@purestorage.com>
2025-02-14 13:44:40 -0800

    update

--- firewall/install_fw2.sh
+++ firewall/install_fw2.sh
@@ -1,2 +1,3 @@
 scp /home/ccrow/personal/homelab/firewall/fw2.sh ubuntu@10.0.1.1:/home/ubuntu/fw2.sh
 ssh ubuntu@10.0.1.1 "sudo /home/ubuntu/fw2.sh"
+ssh ubuntu@10.0.1.1 "sudo iptables-save | sudo tee /etc/iptables/rules.v4"
