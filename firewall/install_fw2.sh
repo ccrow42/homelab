3bb8d413e8fd7219f989b08ac72724bee16251cf
Chris Crow <chris@ccrow.org>
2025-02-18 07:48:16 -0800

    update

--- firewall/install_fw2.sh
+++ firewall/install_fw2.sh
@@ -1,3 +1,4 @@
-scp /home/ccrow/personal/homelab/firewall/fw2.sh ubuntu@10.0.1.1:/home/ubuntu/fw2.sh
+scp /home/ccrow/personal/homelab/firewall/* ubuntu@10.0.1.1:/home/ubuntu/
 ssh ubuntu@10.0.1.1 "sudo /home/ubuntu/fw2.sh"
 ssh ubuntu@10.0.1.1 "sudo iptables-save | sudo tee /etc/iptables/rules.v4"
+#ssh ubuntu@10.0.1.1 "sudo /home/ubuntu/fw2-blocklist.sh"
