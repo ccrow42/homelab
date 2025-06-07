e683cf1be77d3913d281206adc11edfe4834c55c
Chris Crow <ccrow@purestorage.com>
2025-06-06 18:32:07 -0700

    update

--- util/CreateSealedSecretExample.sh
+++ util/CreateSealedSecretExample.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-kubectl apply -f ~/temp/registry-secret.yaml -n heimdall -o yaml --dry-run=client | kubeseal --controller-name=sealed-secrets --controller-namespace=kube-system -o yaml > heimdall/registry-secret-sealed.yaml
+kubectl apply -f ~/temp/registry-secret.yaml -n $1 -o yaml --dry-run=client | kubeseal --controller-name=sealed-secrets --controller-namespace=kube-system -o yaml > ~/personal/homelab/manifests/$1/registry-secret-sealed.yaml
