d439a57a5cc9891988809ab1788f1351832a5925
Chris Crow <ccrow@purestorage.com>
2025-06-09 09:40:21 -0700

    update

--- firewall/install_fw2.sh
+++ firewall/install_fw2.sh
@@ -1,4 +1,9 @@
 scp ~/personal/homelab/firewall/* ubuntu@10.0.1.1:/home/ubuntu/
-ssh ubuntu@10.0.1.1 "sudo /home/ubuntu/fw2.sh"
+
+if [[ $1 == "enable_vpn" ]]; then
+    ssh ubuntu@10.0.1.1 "sudo /home/ubuntu/fw2.sh enable_vpn"
+else
+    ssh ubuntu@10.0.1.1 "sudo /home/ubuntu/fw2.sh"
+fi
 ssh ubuntu@10.0.1.1 "sudo iptables-save | sudo tee /etc/iptables/rules.v4"
 #ssh ubuntu@10.0.1.1 "sudo /home/ubuntu/fw2-blocklist.sh"
