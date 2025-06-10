6c7f2fd896ad13fc6ab8651803b23d3089f1a6f3
Chris Crow <ccrow@purestorage.com>
2025-06-09 17:02:05 -0700

    removed firewall scripts

--- firewall/install_fw2.sh
+++
@@ -1,9 +0,0 @@
-scp ~/personal/homelab/firewall/* ubuntu@10.0.1.1:/home/ubuntu/
-
-if [[ $1 == "enable_vpn" ]]; then
-    ssh ubuntu@10.0.1.1 "sudo /home/ubuntu/fw2.sh enable_vpn"
-else
-    ssh ubuntu@10.0.1.1 "sudo /home/ubuntu/fw2.sh"
-fi
-ssh ubuntu@10.0.1.1 "sudo iptables-save | sudo tee /etc/iptables/rules.v4"
-#ssh ubuntu@10.0.1.1 "sudo /home/ubuntu/fw2-blocklist.sh"
